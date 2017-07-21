0b05d50762d7e1ef8b41011742646d79320d67e0
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-21 10:05:01 -0700

    Complete Specification 2: User is able to order a pizza with 2 toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
+function Pizza(size) {
+  this.toppings = [];
+  this.size = size;
+}
+
 $(function() {
   $("#pizzaOrderForm").submit(function(event) {
     event.preventDefault();
+    $(".order-display").empty();
+
+    var yourPizza = new Pizza();
 
     $("input:checkbox[name=pizza-toppings]:checked").each(function() {
+
       var yourToppings = $(this).val();
-      console.log(yourToppings);
+      yourPizza.toppings.push(yourToppings);
+    });
       $(".order-placeholder").hide();
       $(".order-display").show();
-      $(".order-display").append("<h3>You have ordered a pizza with " + yourToppings + ".</h3></br>")
-    });
+      $(".order-display").append("<h3>You have ordered a </h3>");
+      yourPizza.toppings.forEach(function(element) {
+        $(".order-display").append("<h3>" + element + " </h3>");
+      });
+    $(".order-display").append("<h3>pizza.</h3>");
+
   });
 });
